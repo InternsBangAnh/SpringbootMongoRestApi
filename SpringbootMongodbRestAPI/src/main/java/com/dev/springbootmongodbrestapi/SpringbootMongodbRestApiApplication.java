4e490135db35d35e37c70022375e42e9a0071e50
anhbang97 <anhbangluckystar@gmail.com>
2023-02-27 15:58:11 +0700

    feat: Add Maven Springboot Project - developer

---
+++ SpringbootMongodbRestAPI/src/main/java/com/dev/springbootmongodbrestapi/SpringbootMongodbRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.springbootmongodbrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongodbRestApiApplication.class, args);
+    }
+
+}
